f1bebf776ff05213cd7394e96c195ea2755e948d
Joy Ahmed <dev.joyahmed@gmail.com>
2024-06-26 00:42:54 +0600

    api call with tsq

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,43 @@
-import { useEffect, useState } from "react";
+// src/App.tsx
 import { fetchData } from "./services/api";
 import { Product } from "./types/Product";
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [error, setError] = useState<string | null>(null);
+  const {
+    data: products,
+    error,
+    isLoading,
+    isError,
+  } = useQuery<Product[]>({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const data = await fetchData<Product[]>("products");
+      return data;
+    },
+  });
 
-  useEffect(() => {
-    const getProduct = async () => {
-      try {
-        const data = await fetchData<Product[]>("products");
-        setProducts(data);
-      } catch (error: any) {
-        setError(error.message);
-      }
-    };
-    getProduct();
-  }, []);
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center -z-10">
+        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Error: {(error as Error).message}</div>;
+  }
 
   return (
     <>
       <h1 className="font-bold text-3xl">Hi Mom.</h1>
-      <pre>{JSON.stringify(products, null, 4)}</pre>
-      {products.map((product) => (
+      {products?.map((product) => (
         <div key={product.id}>
-          <h1>{product.title}</h1>
+          <h1 className="text-2xl font-bold">{product.title}</h1>
+          <p>{product.description}</p>
+          <p>{product.price}</p>
         </div>
       ))}
     </>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,18 +1,13 @@
+// src/services/api.ts
 import axios from "axios";
-import { ApiResponse } from "../types/ApiResponse";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api",
 });
 
-export const fetchData = async <T>(
-  endpoint: string
-): Promise<ApiResponse<T>> => {
-  try {
-    const response = await api.get(`/${endpoint}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data", error);
-    throw error;
-  }
+export const fetchData = async <T>(endpoint: string): Promise<T> => {
+  const response = await api.get<T>(`/${endpoint}`);
+  return response.data;
 };
+
+export default api;
